595c3cea8b72fd7e1e4af73e3074a5fdfbf59f6f
TriNguyen <triminh20020412@gmail.com>
2022-08-25 15:18:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myroute_cs426_finalproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myroute_cs426_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
